95c07d471f32ae56f86144ae19a627c943f2d99e
rin <tiawatkins79@gmail.com>
2026-04-02 22:16:21 -0400

    confim demension type

--- source_code/init.py
+++ source_code/init.py
@@ -9,9 +9,11 @@ def gather_user_pref():
     print("AltiusVista setup")
 
     preferred_plot_type = input("Input the plot type that you would like to use (e.g., line, bar, scatter): ")
-    
+    preferred_dem_type = input("Input the demension type that you would like to use (e.g. 2D, 2.5D, 3D...): ")
+
     user_preferences = {
-        "plot_type": preferred_plot_type
+        "plot_type": preferred_plot_type,
+        "dem_type": preferred_dem_type
     }
     
     print(f"Your preferences have been saved.")
