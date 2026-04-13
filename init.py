f5f3af7cd03d2c69f7ba4930fe0ed8b9d5126dea
rin <tiawatkins79@gmail.com>
2026-04-13 16:00:13 -0400

    Create installation program for Linux, create graphing engine file, etc.

---
+++ init.py
@@ -0,0 +1,49 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly as py
+import pandas as pd
+import json
+
+def gather_user_pref():
+
+    print("AltiusVista setup")
+
+    preferred_plot_type = input("Input the plot type that you would like to use (e.g., line, bar, scatter, hist): ")
+    preferred_dem_type = input("Input the demension type that you would like to use (e.g. 2D, 2.5D, 3D...): ")
+    preferred_color = input("Input the color that you would like to use (RGBA, Hex): ")
+
+    print("Graph titles")
+
+    main_title = input("Input your title for this graph: ")
+    x_axi_label = input("Input your x-axis label: ")
+    y_axi_label = input("Input your y-axis label: ")
+
+    print("Graph data")
+
+    x_data = input("Input the data for your x-axis: ")
+    y_data = input("Input the data for your y-axis: ")
+
+    user_preferences = {
+        "plot_type": preferred_plot_type,
+        "dem_type": preferred_dem_type,
+        "color": preferred_color,
+        "title": main_title,
+        "x-label": x_axi_label,
+        "y-label": y_axi_label
+    }
+
+    graph_data = {
+        "x-data": x_data,
+        "y-data": y_data
+    }
+
+    save_data = (json.dumps(user_preferences), json.dumps(graph_data))
+
+    f = open("save_data.json", "w")
+    json.dump(save_data, f)
+    f.close()
+
+    print(f"Your preferences, titles, and data has been saved.")
+    return user_preferences, graph_data
+
+gather_user_pref()
---
+++ source_code/main_engine/data_graph.py
@@ -0,0 +1,4 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly as py
+import pandas as pd
