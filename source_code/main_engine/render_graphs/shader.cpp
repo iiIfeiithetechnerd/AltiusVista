5a40d40285cd987d4903c4c5be18bf6fd632bb38
Rin_Amxyhtst <tiawatkins79@gmail.com>
2026-04-13 20:29:35 -0400

    update cpp file

--- source_code/main_engine/render_graphs/shader.cpp
+++ source_code/main_engine/render_graphs/shader.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <vulkan/vulkan.hpp>
+#include "shader.hpp"
+
+std::vector<char> readFile(const std::string& filename) {
+
+    std::ifstream file(filename, std::ios::ate | std::ios::binary); 
+    std::vector<char> buffer(file.tellg());
+    file.seekg(0);
+    file.read(buffer.data(), buffer.size());        
+    return buffer;
+}
