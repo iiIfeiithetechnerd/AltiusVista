0a9257551dcb17a6cf79e44c29449604b8e0fbdf
Rin_Amxyhtst <tiawatkins79@gmail.com>
2026-05-13 17:14:06 -0400

    add automated makefile build process

---
+++ source_code/lin_install_libraries.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void install_libraries() {
+
+    #ifdef __linux__
+
+        system("pip install -r requirements.txt");
+
+    #else
+
+        system("venv\\Scripts\\pip install -r requirements.txt");
+
+    #endif
+
+}
+
+int main() {
+
+    install_libraries();
+    return 0;
+    
+}
