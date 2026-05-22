dec0a6f63690399eb79b549f2730d2b8de340057
Rin_Amxyhtst <tiawatkins79@gmail.com>
2026-05-22 11:35:51 -0400

    fix path for linux

--- source_code/lin_install_libraries.c
+++ source_code/lin_install_libraries.c
@@ -9,7 +9,7 @@ void install_libraries() {
 
     #ifdef __linux__
 
-    system("./.venv/bin/pip install -r requirements.txt");
+    system("python3 -m pip install -r requirements.txt");
 
     #else
 
